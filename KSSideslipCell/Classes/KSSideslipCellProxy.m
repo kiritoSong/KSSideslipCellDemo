f63a9b6d86fc31a5b651eb7566feabd384b142f2
Klaus <klaus.liu@geekbang.com>
2019-05-09 12:02:50 +0800

    asd

--- KSSideslipCell/Classes/KSSideslipCellProxy.m
+++ KSSideslipCell/Classes/KSSideslipCellProxy.m
@@ -19,7 +19,7 @@
 @end
 
 
-@interface KTSideslipCellProxy()<UIScrollViewDelegate,UITableViewDataSource>
+@interface KTSideslipCellProxy()
 @property (nonatomic,weak) id<UIScrollViewDelegate,UITableViewDelegate> tbDelegate;
 @property (nonatomic,weak) id<UITableViewDataSource> tbDataSource;
 @end
@@ -27,73 +27,23 @@
 @implementation KTSideslipCellProxy
 
 - (BOOL)respondsToSelector:(SEL)aSelector {
-    return [self.target respondsToSelector:aSelector] ||[self.tbDataSource respondsToSelector:aSelector] ;
+    if (aSelector == @selector(scrollViewWillBeginDragging:) || aSelector == @selector(tableView:didSelectRowAtIndexPath:)) {
+        return YES;
+    }
+    BOOL res = [self.tbDelegate respondsToSelector:aSelector] ||[self.tbDataSource respondsToSelector:aSelector];
+    return res ;
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    if ([self.tbDataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
-        return [self.tbDataSource numberOfSectionsInTableView:tableView];
+- (BOOL)isKindOfClass:(Class)aClass {
+    if ([NSStringFromClass(aClass) isEqualToString:@"KTSideslipCellProxy"]) {
+        return YES;
+    }else {
+        return NO;
     }
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if ([self.tbDataSource respondsToSelector:@selector(tableView:numberOfRowsInSection:)]) {
-        return [self.tbDataSource tableView:tableView numberOfRowsInSection:section];
-    }
-    return 0;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:heightForRowAtIndexPath:)]) {
-        return [self.tbDelegate tableView:tableView heightForRowAtIndexPath:indexPath];
-    }
-    return 44;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if ([self.tbDelegate respondsToSelector:@selector(cellForRowAtIndexPath:)]) {
-        return [self.tbDataSource tableView:tableView cellForRowAtIndexPath:indexPath];
-    }
-    return nil;
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:willDisplayCell:forRowAtIndexPath:)]) {
-        return [self.tbDelegate tableView:tableView willDisplayCell:cell forRowAtIndexPath:indexPath];
-    }
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:heightForHeaderInSection:)]) {
-        return [self.tbDelegate tableView:tableView heightForHeaderInSection:section];
-    }
-    return CGFLOAT_MIN;
-}
-
-- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:viewForFooterInSection:)]) {
-        return [self.tbDelegate tableView:tableView viewForFooterInSection:section];
-    }
-    return [UIView new];
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:heightForFooterInSection:)]) {
-        return [self.tbDelegate tableView:tableView heightForFooterInSection:section];
-    }
-    return CGFLOAT_MIN;
-}
-
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    if ([self.tbDelegate respondsToSelector:@selector(tableView:viewForHeaderInSection:)]) {
-        return [self.tbDelegate tableView:tableView viewForHeaderInSection:section];
-    }
-    return [UIView new];
 }
 
 -(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-  
+    
     if (self.target.sideslip) {
         [self.target hiddenAllSideslip];
     }
@@ -116,16 +66,15 @@
 
 - (id)forwardingTargetForSelector:(SEL)aSelector
 {
-    return self.target;
+    id res;
+    if ([self.tbDelegate respondsToSelector:aSelector]) {
+        res = self.tbDelegate;
+    }else if ([self.tbDataSource respondsToSelector:aSelector]) {
+        res = self.tbDataSource;
+    }
+    return res;
 }
 
-- (BOOL)isKindOfClass:(Class)aClass {
-    if ([NSStringFromClass(aClass) isEqualToString:@"KTSideslipCellProxy"]) {
-        return YES;
-    }else {
-        return NO;
-    }
-}
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)sel {
     return nil;
